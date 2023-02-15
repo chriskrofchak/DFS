ef148a4205db2fe26ffbfb95c06b94b8a3bc73c5
cikrofch <cikrofch@uwaterloo.ca>
2023-02-14 21:16:02 -0500

    added tests folder

---
+++ watdfs_server.d
@@ -0,0 +1,4 @@
+watdfs_server.o: watdfs_server.cpp watdfs_make_args.h rpc.h debug.h \
+ /usr/include/fuse/fuse.h /usr/include/fuse/fuse_common.h \
+ /usr/include/fuse/fuse_opt.h /usr/include/fuse/fuse_common_compat.h \
+ /usr/include/fuse/fuse_compat.h
