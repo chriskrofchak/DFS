f8e270d0f14f4874541129a2e65fb49e40f560c7
Christopher Ian Krofchak <cikrofchak@uwaterloo.ca>
2023-02-15 09:55:23 -0500

    added test python

---
+++ tests/long_write.py
@@ -0,0 +1,26 @@
+import os 
+
+LEN = 65535
+SIZE = 2*LEN + 500
+
+user_ = os.environ.get("USER")
+fd = os.open("/tmp/" + user_ + "/mount/long_write.txt", os.O_RDWR|os.O_CREAT)
+print("Opened /mount/long_write successfully with fd", fd)
+
+test_string = 'z'*SIZE
+res = os.write(fd, bytes(test_string, 'utf-8'))
+if (res == SIZE):
+    print("Successfully wrote:", res, "bytes.")
+else:
+    print("Wrote:", res, "bytes, but this is not", SIZE)
+
+# print(os.stat("/tmp/" + user_ + "/mount/open_write.txt"))
+
+res = os.pread(fd, SIZE, 0)
+if (res == test_string):
+    print("Successfully read test_string")
+else:
+    print(res)
+
+os.close(fd)
+print("Successfully closed file")
--- tests/write.py
+++ tests/write.py
@@ -4,13 +4,13 @@ import os
 user_ = os.environ.get("USER")
 fd = os.open("/tmp/" + user_ + "/mount/open_write.txt", os.O_RDWR|os.O_CREAT)
 print("Opened /mount/open_write successfully with fd", fd)
-res = os.write(fd, bytes("aaaaaaa", 'utf-8'))
+res = os.write(fd, bytes("abababa", 'utf-8'))
 print("Successfully wrote:", res, "bytes.")
 
 print(os.stat("/tmp/" + user_ + "/mount/open_write.txt"))
 
-res = os.pread(fd, 7, 0)
 
+res = os.pread(fd, 7, 0)
 print(res)
 
 os.close(fd)
