ba4be8071588ecd573703dd918f44ccdd5205db9
Christopher Ian Krofchak <cikrofchak@uwaterloo.ca>
2023-03-05 16:19:13 -0500

    fsync maybe doesnt work wronly

--- a3_tests/marm/pub_fsync.py
+++ a3_tests/marm/pub_fsync.py
@@ -9,17 +9,17 @@ make_filename = lambda: str(uuid.uuid4())[:8] + ".txt"
 user_ = os.environ.get("USER")
 mountpath_ = "/tmp/" + user_ + "/mount/"
 filename_ = mountpath_ + make_filename()
-fd = os.open(filename_, os.O_RDWR|os.O_CREAT)
+fd = os.open(filename_, os.O_WRONLY|os.O_CREAT)
 print("Opened", filename_, "successfully with fd", fd)
 
 res = os.write(fd, bytes("abababa", 'utf-8'))
 print("Successfully wrote:", res, "bytes.")
 
-print("Stat, to check size:")
-print(os.stat(filename_))
+# print("Stat, to check size:")
+# print(os.stat(filename_))
 
-res = os.pread(fd, 7, 0)
-print(res)
+# res = os.pread(fd, 7, 0)
+# print(res)
 
 os.fsync(fd)
 print("Successfully flushed to server with fsync")
